3f37f37addfb6ee59ad53303edbaeea62fbb0bc1
Vidya <vidya1985@gmail.com>
2024-07-07 14:27:26 +0530

    learning branches

---
+++ AccessSpecifier_Class02.java
@@ -0,0 +1,20 @@
+package AccessSpecifier_Package;
+public class AccessSpecifier_Class02
+{
+	public	void add()
+	{
+		
+	}
+protected void sub()
+	{
+		
+	}
+private	void mul()
+	{
+		
+	}
+	void Div()
+	{
+		
+	}
+}
---
+++ Acessspecifier_Class01.java
@@ -0,0 +1,11 @@
+package AccessSpecifier_Package;
+public class Acessspecifier_Class01 
+{
+public static void main(String[] args) 
+{
+	AccessSpecifier_Class02 a1= new AccessSpecifier_Class02();
+	a1.add();
+	a1.sub();
+	a1.Div();
+}
+}
